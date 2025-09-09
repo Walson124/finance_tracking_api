07107cf0da39edc68ac12966e05ea26d169fa6f3
Walson124 <walson124@gmail.com>
2025-09-09 09:05:59 -0400

    adjusted stack return to better fit mui chart reqs

--- app/routes/analysis/stack_chart/stack_chart_service.py
+++ app/routes/analysis/stack_chart/stack_chart_service.py
@@ -8,7 +8,16 @@ def get_data(connector):
         )
         result = connector.run_query(query)
         result = [{'year': r[0], 'month': r[1], 'category': r[2], 'sum': r[3]} for r in result]
-        return result
+
+        # for stack by category we want category: [sum_year_mo1, sum_year_mo2, ...]
+        # if year_mo doesn't exist for given category, sum_year_mo should be 0
+        distinct_ym = sorted({(r["year"], r["month"]) for r in result})
+        stacked = {r["category"]: [0] * len(distinct_ym) for r in result}
+        for r in result:
+            idx = distinct_ym.index((r["year"], r["month"]))
+            stacked[r["category"]][idx] = r["sum"]
+
+        return stacked
     except Exception as e:
         print(e)
     return []
