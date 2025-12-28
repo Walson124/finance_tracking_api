2d51484d3263ddb363410d34858c28ded17a8338
Walson <walson124@gmail.com>
2025-12-28 18:16:48 -0500

    adjusted backend to make obj with str month

--- app/routes/insert/get_income/get_income_service.py
+++ app/routes/insert/get_income/get_income_service.py
@@ -9,13 +9,13 @@ def run(connector):
     output = {}
     for row in rows:
         person_nm = row[0]
-        month_name = row[1]
+        month_name = row[1] # "January", ..., "December"
         year = row[2]
         income = row[3]
         if person_nm not in output:
             output[person_nm] = {}
         if year not in output[person_nm]:
             output[person_nm][year] = [0.0] * 12
-        output[person_nm][year][month_name - 1] = float(income)
+        output[person_nm][year][month_name] = float(income)
 
     return output
