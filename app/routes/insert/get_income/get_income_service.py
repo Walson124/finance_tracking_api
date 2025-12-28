722cf9bfe7b87830e8f06e61ff30b51071e30bb2
Walson <walson124@gmail.com>
2025-12-28 18:24:25 -0500

    fix

--- app/routes/insert/get_income/get_income_service.py
+++ app/routes/insert/get_income/get_income_service.py
@@ -1,21 +1,27 @@
+import calendar
+
+MONTHS = list(calendar.month_name)[1:]  # ["January", ..., "December"]
+
+def blank_year_dict():
+    return {m: 0.0 for m in MONTHS}
+
 def run(connector):
-    rows = connector.run_query(
-        """
+    rows = connector.run_query("""
         SELECT person_nm, month, year, income
         FROM financial.income
-    """
-    )
+    """)
 
     output = {}
-    for row in rows:
-        person_nm = row[0]
-        month_name = row[1] # "January", ..., "December"
-        year = row[2]
-        income = row[3]
+    for person_nm, month_name, year, income in rows:
+        year = int(year)
+
         if person_nm not in output:
             output[person_nm] = {}
         if year not in output[person_nm]:
-            output[person_nm][year] = [0.0] * 12
-        output[person_nm][year][month_name] = float(income)
+            output[person_nm][year] = blank_year_dict()
+
+        # only set known months
+        if month_name in output[person_nm][year]:
+            output[person_nm][year][month_name] = float(income or 0)
 
     return output
